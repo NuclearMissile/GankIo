d78e760479a7db213533e5e1f967426de41fca99
torri <torrificwewe@outlook.com>
2017-10-31 10:59:57 +0800

    a

--- app/src/main/java/nuclearr/com/gankio/Util/ImageLoader/GlideImageLoader.java
+++ app/src/main/java/nuclearr/com/gankio/Util/ImageLoader/GlideImageLoader.java
@@ -3,18 +3,20 @@ package nuclearr.com.gankio.Util.ImageLoader;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 
+import com.bumptech.glide.Priority;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.transition.Transition;
 
-import nuclearr.com.gankio.R;
+import nuclearr.com.gankio.BaseApplication;
 import nuclearr.com.gankio.Util.NetworkUtil;
 import nuclearr.com.gankio.Util.SettingUtil;
 
 final class GlideImageLoader {
+
     void loadImage(ImageRequest request) {
-        Context context = request.getImageView().getContext();
+        Context context = BaseApplication.getInstance();
         if (NetworkUtil.isWifiConnected(context)) {
             loadNet(context, request);
             return;
@@ -32,12 +34,13 @@ final class GlideImageLoader {
                 .placeholder(request.getPlaceHolder())
                 .error(request.getError())
                 .onlyRetrieveFromCache(true)
-                .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                .priority(Priority.IMMEDIATE)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .transition(DrawableTransitionOptions.withCrossFade(300))
                 .into(request.getImageView());
     }
 
-/*    private void loadNet(Context context, ImageRequest request) {
+    /*private void loadNet(Context context, ImageRequest request) {
         GlideApp.with(context)
                 .load(request.getUrl())
                 .placeholder(request.getPlaceHolder())
@@ -48,22 +51,20 @@ final class GlideImageLoader {
                 .into(request.getImageView());
     }*/
 
-    // todo fix image load order
+    // todo fix image load order problem
     private void loadNet(Context context, ImageRequest request) {
-        String url1 = request.getUrl();
-        String url2 = (String) request.getImageView().getTag(R.id.image_id);
-        if (url1.equals(url2))
-            GlideApp.with(context)
-                    .load(request.getUrl())
-                    .placeholder(request.getPlaceHolder())
-                    .error(request.getError())
-                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
-                    .transition(DrawableTransitionOptions.withCrossFade(300))
-                    .into(new SimpleTarget<Drawable>() {
-                        @Override
-                        public void onResourceReady(Drawable resource, Transition<? super Drawable> transition) {
-                            request.getImageView().setImageDrawable(resource);
-                        }
-                    });
+        GlideApp.with(context)
+                .load(request.getUrl())
+                .placeholder(request.getPlaceHolder())
+                .error(request.getError())
+                .priority(Priority.IMMEDIATE)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .transition(DrawableTransitionOptions.withCrossFade(300))
+                .into(new SimpleTarget<Drawable>() {
+                    @Override
+                    public void onResourceReady(Drawable resource, Transition<? super Drawable> transition) {
+                        request.getImageView().setImageDrawable(resource);
+                    }
+                });
     }
 }
