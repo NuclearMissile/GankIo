c62d41f2d88e4d16596f7f420663e1822926349b
torri <torrificwewe@outlook.com>
2017-11-06 17:54:47 +0800

    a

--- app/src/main/java/nuclearr/com/gankio/Util/ImageLoader/ImageLoader.java
+++ app/src/main/java/nuclearr/com/gankio/Util/ImageLoader/ImageLoader.java
@@ -2,6 +2,7 @@ package nuclearr.com.gankio.Util.ImageLoader;
 
 import android.widget.ImageView;
 
+import nuclearr.com.gankio.BaseApplication;
 import nuclearr.com.gankio.R;
 
 public final class ImageLoader {
@@ -17,7 +18,16 @@ public final class ImageLoader {
     }
 
     public static void showImage(ImageView imageView, String url) {
-        getFactory().loadImage(new ImageRequest.Builder()
+        getFactory().loadNet(BaseApplication.getInstance(), new ImageRequest.Builder()
+                .url(url)
+                .imgView(imageView)
+                .placeHolder(R.drawable.default_img)
+                .error(R.color.md_red_400)
+                .create());
+    }
+
+    public static void showImage2(ImageView imageView, String url) {
+        getFactory().loadNet2(BaseApplication.getInstance(), new ImageRequest.Builder()
                 .url(url)
                 .imgView(imageView)
                 .placeHolder(R.drawable.default_img)
